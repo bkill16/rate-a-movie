d7a9d8a5a75273061ef7cd609d5a835fe0e16e77
Brenleigh Killpack <kil21008@byui.edu>
2024-06-27 14:00:56 -0600

    added back reviews

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require("express").Router();
 
 routes.use("/users", require("./users"));
-// routes.use("/review", require("./review"));
+routes.use("/review", require("./review"));
 routes.use("/actors", require("./actors"));
 
 routes.get("/", (req, res) => {
