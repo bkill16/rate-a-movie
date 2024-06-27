e1a45526fe32c7f534b44e32f6d799e07bcc5e8e
Brenleigh Killpack <kil21008@byui.edu>
2024-06-27 13:59:22 -0600

    added back reviews

--- routes/review.js
+++ routes/review.js
@@ -1,33 +1,33 @@
-// const express = require('express');
-// const router = express.Router();
-// const reviewsController = require('../controllers/reviews');
+const express = require('express');
+const router = express.Router();
+const reviewsController = require('../controllers/reviews');
 
-// // Route to get all reviews
-// // http://localhost:8080/reviews
-// router.get('/', reviewsController.getAll);
+// Route to get all reviews
+// http://localhost:8080/reviews
+router.get('/', reviewsController.getAllReviews);
 
-// // Route to get a single review by review ID
-// // http://localhost:8080/reviews/reviewId
-// router.get('/review/:reviewId', reviewsController.getSingle);
+// Route to get a single review by review ID
+// http://localhost:8080/reviews/reviewId
+router.get('/review/:reviewId', reviewsController.getSingleReviews);
 
-// // Route to get reviews by user ID
-// // http://localhost:8080/reviews/user/userId
-// router.get('/user/:userId', reviewsController.getByUserId);
+// Route to get reviews by user ID
+// http://localhost:8080/reviews/user/userId
+router.get('/user/:userId', reviewsController.getReviewsByUserId);
 
-// // Route to get reviews by movie ID
-// // http://localhost:8080/reviews/movie/movieId
-// router.get('/movie/:movieId', reviewsController.getByMovieId);
+// Route to get reviews by movie ID
+// http://localhost:8080/reviews/movie/movieId
+router.get('/movie/:movieId', reviewsController.getReviewsByMovieId);
 
-// // Route to create a new review
-// // http://localhost:8080/reviews
-// router.post('/', reviewsController.createReview);
+// Route to create a new review
+// http://localhost:8080/reviews
+router.post('/', reviewsController.createNewReview);
 
-// // Route to update a review by review ID
-// // http://localhost:8080/reviews/reviewId
-// router.put('/review/:reviewId', reviewsController.updateReview);
+// Route to update a review by review ID
+// http://localhost:8080/reviews/reviewId
+router.put('/update/:reviewId', reviewsController.updateReview);
 
-// // Route to delete a review by review ID
-// // http://localhost:8080/reviews/reviewId
-// router.delete('/review/:reviewId', reviewsController.deleteReview);
+// Route to delete a review by review ID
+// http://localhost:8080/reviews/reviewId
+router.delete('/delete/:reviewId', reviewsController.deleteReview);
 
-// module.exports = router;
+module.exports = router;
