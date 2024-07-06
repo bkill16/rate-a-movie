5af28aa46ebdf0f37faa1b3b6216ecd61f83b9d2
TyroneMartin <tyrone306@byui.edu>
2024-07-06 15:41:58 -0600

    The additional validation directory was removed, and the validation rules that was created for movies was places in the util/validation direcory and the middle was added to the post and put routes for movies

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const moviesController = require("../controllers/movies");
+const {movieValidationRules, validateMovie} = require('../util/validation/movieValidator');
+
+
 
 router.get("/", moviesController.getAllMovies);
 router.get("/:id", moviesController.getSingleMovie);
 router.get("/title/:title", moviesController.getMovieByTitle);
 router.get("/actor/:name", moviesController.getMovieByActorName);
-router.post("/", moviesController.createMovie);
-router.put("/:id", moviesController.updateMovie);
+
+// Routes with validation
+router.post("/", movieValidationRules, validateMovie, moviesController.createMovie);
+router.put("/:id", movieValidationRules, validateMovie, moviesController.updateMovie);
+
 router.delete("/:id", moviesController.deleteMovie);
 
 module.exports = router;
