0dde940de96071afff8d3e07d3991b959fbd7513
Brenleigh Killpack <kil21008@byui.edu>
2024-07-11 11:49:17 -0600

    movieactor test got overwritten - fixed

--- __tests__/movieactor.test.js
+++ __tests__/movieactor.test.js
@@ -6,12 +6,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const MONGODB_TEST_URI = process.env.MONGODB_TEST_URI;
-const Review = require("../models/Reviews");
-const Movie = require("../models/Movie");
+const MovieActor = require("../models/MovieActor");
 const startServer = require("../server");
 
 let app;
-let server;
 
 beforeAll(async () => {
   app = await startServer();
@@ -20,8 +18,7 @@ beforeAll(async () => {
 });
 
 afterEach(async () => {
-  await Review.deleteMany({});
-  await Movie.deleteMany({});
+  await MovieActor.deleteMany({});
 });
 
 afterAll(async () => {
@@ -31,168 +28,111 @@ afterAll(async () => {
   await new Promise((resolve) => setTimeout(resolve, 500));
 });
 
-describe("Rate a Movie API - Reviews", () => {
-  it("should get all reviews", async () => {
-    const movie = new Movie({
-      title: "Star Wars",
-      director: "George Lucas",
-      production_company: "Lucasfilm",
-      distribution_company: "20th Century Fox",
-      US_release_date: "1977-05-25",
-      running_time: 121,
-      audience_rating: "PG",
+describe("Rate a Movie API - MovieActors", () => {
+  it("should get all movie actor relationships", async () => {
+    const relationship1 = new MovieActor({
+      movieId: "667b1de0f592b5c99c067fc3",
+      actorId: "667ca138ea8a0e7bbff67a12",
+      role: "Chewbacca",
     });
-    await movie.save();
+    const relationship2 = new MovieActor({
+      movieId: "667b1de0f592b5c99c067fc8",
+      actorId: "667ca138ea8a0e7bbff67a0a",
+      role: "Boba Fett",
+    });
+    const relationship3 = new MovieActor({
+      movieId: "667b1de0f592b5c99c067fc3",
+      actorId: "667ca138ea8a0e7bbff67a13",
+      role: "Jabba the Hutt",
+    });
+    await relationship1.save();
+    await relationship2.save();
+    await relationship3.save();
 
-    const review1 = new Review({ movie_id: movie._id, rating: 4, comment: "Great movie!" });
-    const review2 = new Review({ movie_id: movie._id, rating: 5, comment: "Excellent!" });
-    await review1.save();
-    await review2.save();
-
-    const response = await request(app).get("/reviews");
+    const response = await request(app).get("/movie-actors");
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.length).toBe(2);
+    expect(response.body.length).toBe(3);
 
-    expect(response.body[0].rating).toBe(4);
-    expect(response.body[0].comment).toBe("Great movie!");
+    expect(response.body[0].movieId).toBe("667b1de0f592b5c99c067fc3");
+    expect(response.body[0].actorId).toBe("667ca138ea8a0e7bbff67a12");
+    expect(response.body[0].role).toBe("Chewbacca");
 
-    expect(response.body[1].rating).toBe(5);
-    expect(response.body[1].comment).toBe("Excellent!");
+    expect(response.body[1].movieId).toBe("667b1de0f592b5c99c067fc8");
+    expect(response.body[1].actorId).toBe("667ca138ea8a0e7bbff67a0a");
+    expect(response.body[1].role).toBe("Boba Fett");
+
+    expect(response.body[2].movieId).toBe("667b1de0f592b5c99c067fc3");
+    expect(response.body[2].actorId).toBe("667ca138ea8a0e7bbff67a13");
+    expect(response.body[2].role).toBe("Jabba the Hutt");
   });
 
-  it("should get a single review by ID", async () => {
-    const movie = new Movie({
-      title: "Star Wars",
-      director: "George Lucas",
-      production_company: "Lucasfilm",
-      distribution_company: "20th Century Fox",
-      US_release_date: "1977-05-25",
-      running_time: 121,
-      audience_rating: "PG",
-    });
-    await movie.save();
-
-    const review = new Review({ movie_id: movie._id, rating: 4, comment: "Great movie!" });
-    await review.save();
-
-    const response = await request(app).get(`/reviews/${review._id}`);
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body.rating).toBe(4);
-    expect(response.body.comment).toBe("Great movie!");
-  });
-
-  it("should get reviews by movie title", async () => {
-    const movie = new Movie({
-      title: "Star Wars",
-      director: "George Lucas",
-      production_company: "Lucasfilm",
-      distribution_company: "20th Century Fox",
-      US_release_date: "1977-05-25",
-      running_time: 121,
-      audience_rating: "PG",
-    });
-    await movie.save();
-
-    const review = new Review({ movie_id: movie._id, rating: 5, comment: "Amazing!" });
-    await review.save();
-
-    const response = await request(app).get(`/reviews/title/${encodeURIComponent(movie.title)}`);
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body.movie).toBe("Star Wars");
-    expect(response.body.reviews.length).toBe(1);
-    expect(response.body.reviews[0].comment).toBe("Amazing!");
-  });
-
-  it("should create a new review", async () => {
-    const movie = new Movie({
-      title: "Star Wars",
-      director: "George Lucas",
-      production_company: "Lucasfilm",
-      distribution_company: "20th Century Fox",
-      US_release_date: "1977-05-25",
-      running_time: 121,
-      audience_rating: "PG",
-    });
-    await movie.save();
-
-    const response = await request(app).post("/reviews").send({
-      movie_id: movie._id,
-      rating: 5,
-      comment: "Excellent!"
+  it("should create a new movie actor relationship", async () => {
+    const response = await request(app).post("/movie-actors").send({
+      movieId: "667b1de0f592b5c99c067fc8",
+      actorId: "667ca138ea8a0e7bbff67a0a",
+      role: "Boba Fett",
     });
 
     expect(response.statusCode).toBe(201);
-    expect(response.body.rating).toBe(5);
-    expect(response.body.comment).toBe("Excellent!");
+
+    const savedMovieActor = response.body.savedMovieActor;
+    
+    expect(savedMovieActor.movieId).toBe("667b1de0f592b5c99c067fc8");
+    expect(savedMovieActor.actorId).toBe("667ca138ea8a0e7bbff67a0a");
+    expect(savedMovieActor.role).toBe("Boba Fett");
   });
 
-  it("should update a review by ID", async () => {
-    const movie = new Movie({
-      title: "Star Wars",
-      director: "George Lucas",
-      production_company: "Lucasfilm",
-      distribution_company: "20th Century Fox",
-      US_release_date: "1977-05-25",
-      running_time: 121,
-      audience_rating: "PG",
+  it("should update a movie actor relationship by id", async () => {
+    const relationship = new MovieActor({
+      movieId: "667b1de0f592b5c99c067fc3",
+      actorId: "667ca138ea8a0e7bbff67a12",
+      role: "Elrond",
     });
-    await movie.save();
+    await relationship.save();
 
-    const review = new Review({ movie_id: movie._id, rating: 4, comment: "Good movie" });
-    await review.save();
-
-    const response = await request(app).put(`/reviews/${review._id}`).send({
-      movie_id: movie._id,
-      rating: 5,
-      comment: "Great movie!"
-    });
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body.rating).toBe(5);
-    expect(response.body.comment).toBe("Great movie!");
-  });
-
-  it("should delete a review by ID", async () => {
-    const movie = new Movie({
-      title: "Star Wars",
-      director: "George Lucas",
-      production_company: "Lucasfilm",
-      distribution_company: "20th Century Fox",
-      US_release_date: "1977-05-25",
-      running_time: 121,
-      audience_rating: "PG",
-    });
-    await movie.save();
-
-    const review = new Review({ movie_id: movie._id, rating: 3, comment: "Not bad" });
-    await review.save();
-
-    const response = await request(app).delete(`/reviews/${review._id}`);
+    const response = await request(app)
+      .put(`/movie-actors/${relationship._id}`)
+      .send({
+        movieId: "667b1de0f592b5c99c067fc3",
+        actorId: "667ca138ea8a0e7bbff67a0a",
+        role: "Legolas",
+      });
 
     expect(response.statusCode).toBe(204);
   });
 
-  it("should return a 404 for a non-existent review on get", async () => {
-    const response = await request(app).get("/reviews/667ca138ea8a0e7bbff67a14");
-    
-    expect(response.statusCode).toBe(404);
-  });
-
-  it("should return a 404 for a non-existent review on put", async () => {
-    const response = await request(app).put("/reviews/667ca138ea8a0e7bbff67a14").send({
-      movie_id: "667b1de0f592b5c99c067fc3",
-      rating: 4,
-      comment: "Updated comment"
+  it("should delete a movie actor relationship by id", async () => {
+    const relationship = new MovieActor({
+      movieId: "667b1de0f592b5c99c067fc8",
+      actorId: "667ca138ea8a0e7bbff67a0a",
+      role: "Kermit the Frog",
     });
-    
-    expect(response.statusCode).toBe(404);
+    await relationship.save();
+
+    const response = await request(app).delete(
+      `/movie-actors/${relationship._id}`
+    );
+
+    expect(response.statusCode).toBe(204);
   });
 
-  it("should return a 404 for a non-existent review on delete", async () => {
-    const response = await request(app).delete("/reviews/667b1de0f592b5c99c067fcb");
+  it("should return a 404 for a non-existent relationship on put", async () => {
+    const response = await request(app)
+      .put("/movie-actors/667ca138ea8a0e7bbff67a14")
+      .send({
+        movieId: "667b1de0f592b5c99c067fc3",
+        actorId: "667ca138ea8a0e7bbff67a12",
+        role: "Gandalf",
+      });
+    
+      expect(response.statusCode).toBe(404);
+  });
+
+  it("should return a 404 for a non-existent relationship on delete", async () => {
+    const response = await request(app).delete(
+      "/movie-actors/667b1de0f592b5c99c067fcb"
+    );
     
     expect(response.statusCode).toBe(404);
   });
