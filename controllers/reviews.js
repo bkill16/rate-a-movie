533b34a33bbe9f238b782ce50530a37af184d5e6
TyroneMartin <tyrone306@byui.edu>
2024-07-18 23:42:45 -0600

    update

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,5 +1,5 @@
 const Review = require('../models/Reviews');
-const Movie = require('../models/Movie');
+// const Movie = require('../models/Movie');
 const mongoose = require('mongoose');
 
 const getAllReviews = async (req, res) => {
@@ -33,59 +33,6 @@ const getSingleReview = async (req, res) => {
     }
 };
 
-<<<<<<< HEAD
-const getReviewsByTitle = async (req, res) => {
-    try {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        const movieTitle = decodeURIComponent(req.params.title);
-
-        const movie = await Movie.findOne({ 
-            title: { $regex: new RegExp(movieTitle, 'i') }
-        });
-
-        if (!movie) {
-            return res.status(404).json({ message: 'Movie not found' });
-        }
-
-        const reviews = await Review.find({ movie_id: movie._id });
-
-        if (reviews.length === 0) {
-            return res.status(404).json({ 
-                message: 'No reviews found for this movie',
-                movieTitle: movie.title
-            });
-        }
-
-        res.json({
-            movie: movie.title,
-            reviews: reviews
-        });
-    } catch (error) {
-        res.status(500).json({ message: error.message || "Some error occurred while getting reviews by movie title." });
-    }
-};
-
-const getReviewsByUserId = async (req, res) => {
-    try {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        const { userId } = req.params;
-
-        if (!mongoose.Types.ObjectId.isValid(userId)) {
-            return res.status(400).json({ message: 'Must use a valid user id to get reviews.' });
-        }
-
-        const reviews = await Review.find({ user_id: userId });
-
-        if (reviews.length === 0) {
-            return res.status(404).json({ message: 'No reviews found for this user' });
-        }
-
-        res.json(reviews);
-    } catch (error) {
-        res.status(500).json({ message: error.message || "Some error occurred while getting reviews by user ID." });
-    }
-};
-=======
 // const getReviewsByTitle = async (req, res) => {
 //     try {
 //         res.setHeader("Access-Control-Allow-Origin", "*");
@@ -138,7 +85,6 @@ const getReviewsByUserId = async (req, res) => {
 //     }
 // };
 
->>>>>>> fd93740c4396dcc0a095a290ac7ba3ee53e676c8
 
 const createNewReview = async (req, res) => {
     try {
--- routes/review.js
+++ routes/review.js
@@ -3,20 +3,6 @@ const router = express.Router();
 const reviewsController = require('../controllers/reviews');
 const { reviewValidationRules, validateReview } = require('../util/validation/review');
 
-<<<<<<< HEAD
-// Non-validated routes
-router.get('/', reviewsController.getAllReviews);  // http://localhost:3000/reviews
-router.get('/:reviewId', reviewsController.getSingleReview);  // http://localhost:3000/reviews/123456789
-router.get('/title/:title', reviewsController.getReviewsByTitle); // http://localhost:3000/reviews/title/Inception
-router.get('/user/:userId', reviewsController.getReviewsByUserId); // http://localhost:3000/reviews/user/16543456789
-
-// Routes with validation
-router.post('/', reviewValidationRules, validateReview, reviewsController.createNewReview); // http://localhost:3000/reviews
-router.put('/:reviewId', reviewValidationRules, validateReview, reviewsController.updateReview); // http://localhost:3000/reviews
-
-// Delete a review by ID
-router.delete('/:reviewId', reviewsController.deleteReview); // http://localhost:3000/reviews/123456789 
-=======
 router.get('/', reviewsController.getAllReviews);
 router.get('/:reviewId', reviewsController.getSingleReview);
 
@@ -31,6 +17,5 @@ router.delete('/:reviewId', reviewsController.deleteReview);
 // remove routes due to bugs
 // router.get('/title/:title', reviewsController.getReviewsByTitle); 
 // router.get('/user/:userId', reviewsController.getReviewsByUserId);
->>>>>>> fd93740c4396dcc0a095a290ac7ba3ee53e676c8
 
 module.exports = router;
